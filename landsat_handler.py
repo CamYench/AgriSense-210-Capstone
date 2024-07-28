c1a7b5b1644110be4b15e794b48fd60b2f2916e3
Pascual Eley <pascualeley@berkeley.edu>
2024-07-28 15:41:36 -0700

    created local image updater and adjusted handler

--- landsat_handler.py
+++ landsat_handler.py
@@ -13,15 +13,12 @@ import json
 #find most recent landsat images from S3 file (secondary: must cover entire land area)
 #currently assumes that multiple images from the same date do not exist
 
+
+
+
 def retrieve_latest_images():
     # Initialize S3 client and search client
     s3_client = boto3.client('s3')
-    # s3_client = boto3.client(
-    # 's3',
-    # aws_access_key_id='ASIAZSICGRC5IGUMLDVH',
-    # aws_secret_access_key='x3I7whN2rs7GVChQj7cyT/PqUUK6vMvIeN/xwvQh',
-    # aws_session_token='IQoJb3JpZ2luX2VjEN3//////////wEaCXVzLWVhc3QtMSJIMEYCIQD2asuGwqfEfGYk2WHZCxMclU6X4We3XmwOW3Qx45b1RgIhAPGel695fVjm9z79wZrbcfZpwTjc3UU8PplftPlKTiwsKvUCCLX//////////wEQAhoMNjU3NjcxNDg5NzIyIgwYrRGerdTW8tANC1oqyQKeVEuYo2DBaU6yGPQ1chky7iuGptD8TGmH78eEuoxRT0jE1JDOwRjqqxA4sYqrUJCKfNr1xfEUrjU4I9xVgXBrDflZI2kuBOOfZtX+YLZOWVtT4g2zWFTTHA+LEDBMCy9cfOJPDdjUC9n4lJZ3I007aJWt0awL0KTm1hgMBSEaB6CKq5oCCpUJiLqxw5EcjgDuJbQ/JZH4UR8l3giCnWEl8j7DBNq/5FZ8a81oPpFYYfPCq+N/CwQfo0b97me/pdIsuL+G/lZTyK3tjCxNObqdB/pt+aX2L+YDFz6+8CFz1nTPVEVv9ZNhJjzijLSR6VXKryVebyqeRAd3fWDhELtcaZAtJrG90+Ig6UZKee52Pj5dphvayXTsGsVeoO0g20DGv9jyXN635XJZ8zL+c1AVpZAZIFaaLy+dMks3ZPRrPo/OPdo3wGWbiTD+nYe1BjqmAWrSx++3au7LG8RaeWdnLxmW2qMiviTskvXxgDwhZ+0RzYTlf0bSpFdR6qFBA2wN87vvfnHJctemYI3Nutwr8hfBvY1pTP2N4Hv+YobsZu1VR37nE8uBJ8aZ3lM6q0RFCH8VEHqRuOapRjWofIlF0Bgxd3oRJb7lYmf9pOMR/QG7TX6Gj8LDNJyGeBuX35sycViFRQYm8n5sJd64t3yC6P9DLj+7Pks=')
-    
     paginator = s3_client.get_paginator('list_objects_v2')
     page_iterator = paginator.paginate(Bucket="agrisense3", Prefix="converted/")
     page_iterator_mtvi = paginator.paginate(Bucket="agrisense3", Prefix="mtvi2_output")
@@ -80,6 +77,33 @@ def retrieve_latest_images():
     
 
     return (latest_evi, latest_surface_temp, latest_smi, latest_mtvi, most_recent_date)
+
+
+def retrieve_last_4_evi():
+    # Initialize S3 client and search client
+    s3_client = boto3.client('s3')
+    paginator = s3_client.get_paginator('list_objects_v2')
+    page_iterator = paginator.paginate(Bucket="agrisense3", Prefix="converted/")
+    
+    #list of EVI files present
+    evi_files = []
+
+    #iterate through bucket
+    for page in page_iterator:
+        for obj in page.get('Contents', []):
+            key=obj['Key']
+            if 'EVI' in key:
+                try:
+                    date_str = key[27:35]
+                    evi_files.append((date_str, key))
+                except ValueError:
+                    continue
+    
+    evi_files.sort(reverse=True, key=lambda x: x[0])
+    latest_4_evi = [key for date, key in evi_files[:4]]
+
+    return (latest_4_evi)
+
                 
 
 
@@ -116,7 +140,6 @@ def mask_tif(area_to_mask,image_fn):
 
     s3_client = boto3.client('s3')
     #read the file from S3
-    # s3_response = s3_client.get_object(Bucket='agrisense3', Key='converted/'+image_fn)
     s3_response = s3_client.get_object(Bucket='agrisense3', Key=image_fn)   
     # Read the file into a memory file
     with MemoryFile(s3_response['Body'].read()) as memfile:
